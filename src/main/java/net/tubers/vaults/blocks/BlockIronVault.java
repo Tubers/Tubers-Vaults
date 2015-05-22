ccd5cd0c10c37460dfb7b83480bbaca5079dccc9
Liam George <lgeorge4@villanova.edu>
2015-05-22 07:09:31 +0200

    Backup commit.

--- src/main/java/net/tubers/vaults/blocks/BlockIronVault.java
+++ src/main/java/net/tubers/vaults/blocks/BlockIronVault.java
@@ -15,7 +15,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.tubers.vaults.CreativeTab.TubersVaultsTab;
+import net.tubers.vaults.creativetab.TubersVaultsTab;
 import net.tubers.vaults.TubersVaults;
 
 public class BlockIronVault extends BlockContainer {
