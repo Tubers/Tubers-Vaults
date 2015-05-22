2fffc594546ce70de5cff20c37b2df41a6f01271
Liam George <lgeorge4@villanova.edu>
2015-05-22 07:06:00 +0200

    Backup commit.

---
+++ src/main/java/net/tubers/vaults/references/ConfigRef.java
@@ -0,0 +1,12 @@
+package net.tubers.vaults.references;
+
+import net.tubers.vaults.configuration.ConfigHandler;
+
+public class ConfigRef {
+
+
+    public static final int STONE_VAULT_DEFAULT = 2;
+    public static final int STONE_VAULT = ConfigHandler.stoneVault;
+
+
+}
