96b035f1d86d3a488c746d11904578c18e32bad6
Liam George <lgeorge4@villanova.edu>
2015-05-22 07:08:31 +0200

    Backup commit.

--- src/main/java/net/tubers/vaults/references/Reference.java
+++ src/main/java/net/tubers/vaults/references/Reference.java
@@ -5,5 +5,8 @@ public class Reference {
 	public static final String ID = "tubersvaults";
 	public static final String VERSION = "1.7.10-0.0.0";
 	public static final String NAME = "Tubers' Vaults";
+
+	public static final String ClIENT_PATH = "net.tubers.vaults.proxies.ClientProxy";
+	public static final String SERVER_PATH = "net.tubers.vaults.proxies.CommonProxy";
 	
 }
