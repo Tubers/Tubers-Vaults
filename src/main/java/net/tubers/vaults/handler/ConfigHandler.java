207461db0e70ed3c43b09fefe13f276c020ea913
Liam George <lgeorge4@villanova.edu>
2015-05-22 11:36:57 +0200

    Backup commit.

---
+++ src/main/java/net/tubers/vaults/handler/ConfigHandler.java
@@ -0,0 +1,46 @@
+package net.tubers.vaults.handler;
+
+import java.io.File;
+
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
+import net.tubers.vaults.references.ConfigRef;
+import net.tubers.vaults.references.Reference;
+
+public class ConfigHandler {
+
+	public static Configuration config;
+	
+	public static int stoneVault = ConfigRef.STONE_VAULT_DEFAULT;
+
+	public static void init(File file)
+	{
+		if (config == null)
+		{
+			config = new Configuration(file);
+			loadConfiguration();
+		}
+
+	}
+
+	@SubscribeEvent
+	public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+	{
+		if (event.modID.equalsIgnoreCase(Reference.ID))
+		{
+			loadConfiguration();
+		}
+	}
+
+	private static void loadConfiguration()
+	{
+		stoneVault = config.get(Configuration.CATEGORY_GENERAL,
+				"Stone Vault", ConfigRef.STONE_VAULT_DEFAULT,"Comment in config").getInt(ConfigRef.STONE_VAULT_DEFAULT);
+
+		if (config.hasChanged())
+		{
+			config.save();
+		}
+	}
+}
